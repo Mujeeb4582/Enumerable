aae68b8f8844001c390eebd66b4cc15aa9b695d9
Trust7 <simeontrust7@gmail.com>
2023-01-27 13:58:32 +0100

    Add the MyList Class

---
+++ my_list.rb
@@ -0,0 +1,27 @@
+class MyList
+  attr_reader :list
+
+  include MyEnumerable
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5 }
+
+list.all? { |e| e > 5 }
+
+
+list.any? { |e| e == 2 }
+
+list.any? { |e| e == 5 }
+
+list.filter(&:even?)
