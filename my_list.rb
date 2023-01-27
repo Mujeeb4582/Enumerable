b08005555ee63fdc21cdbc00d3c1f6628493d702
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-27 19:52:47 +0500

    Add the suggested code by code reviewer

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,5 @@
+require_relative './myenum'
+
 class MyList
   attr_reader :list
 
@@ -14,12 +16,12 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-list.all? { |e| e < 5 }
+puts(list.all? { |e| e < 5 })
 
-list.all? { |e| e > 5 }
+puts(list.all? { |e| e > 5 })
 
-list.any? { |e| e == 2 }
+puts(list.any? { |e| e == 2 })
 
-list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 5 })
 
-list.filter(&:even?)
+puts list.filter(&:even?)
